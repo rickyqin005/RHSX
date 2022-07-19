a2984f7331770ee9a2bdc72df5dd7091fff59aa3
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2022-07-19 12:02:07 -0400

    modified date formatting

--- index.js
+++ index.js
@@ -889,5 +889,11 @@ function pricef(price) {
 }
 
 function dateString(date) {
-    return date.toLocaleString('en-US', {timeZone: 'America/Toronto'});
+    let year = String(date.getFullYear());
+    let month = String(date.getMonth()+1).padStart(2, '0');
+    let day = String(date.getDate()).padStart(2, '0');
+    let hour = String(date.getHours()).padStart(2, '0');
+    let minute = String(date.getMinutes()).padStart(2, '0');
+    let second = String(date.getSeconds()).padStart(2, '0');
+    return `${year}/${month}/${day} ${hour}:${minute}:${second}`;
 }
