bb1f41cc3cb64a4d65207a5b3eb2bd208dcaf9dd
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2022-09-26 20:37:10 -0400

    added commands to check the bot

---
+++ src/commands/bot/snapshot.js
@@ -0,0 +1,23 @@
+const { Order, Ticker, Trader } = require('../../rhsx');
+const { SlashCommandSubcommandBuilder } = require('@discordjs/builders');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    data: new SlashCommandSubcommandBuilder()
+        .setName('snapshot')
+        .setDescription('Returns the objects currently stored in the bot\'s cache'),
+    ephemeral: true,
+    execute: async function (interaction) {
+        const obj = {
+            timestamp: new Date(),
+            market: global.market,
+            tickers: Array.from(Ticker.cache.values()).map(ticker => ticker.toDBObject()),
+            traders: Array.from(Trader.cache.values()).map(trader => trader.toDBObject()),
+            orders: Array.from(Order.cache.values()).map(order => order.toDBObject())
+        }
+        const fileDir = path.join(__dirname, 'snapshot.json');
+        await fs.writeFile(fileDir, JSON.stringify(obj, undefined, 4), () => {});
+        return { files: [{ attachment: fileDir, name: 'snapshot.json' }] };
+    }
+};
---
+++ src/commands/check.js
@@ -0,0 +1,11 @@
+const { SlashCommandSubcommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandSubcommandBuilder()
+        .setName('check')
+        .setDescription('Check if the bot is alive'),
+    ephemeral: false,
+    execute: async function (interaction) {
+        return { content: `${global.discordClient.user.username} is active!` };
+    }
+};
