abe412bf907a6fa1a4bdcc779fb438cff3157efd
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2022-10-01 20:49:29 -0400

    added more utility functions

--- src/utils/Tools.js
+++ src/utils/Tools.js
@@ -3,7 +3,23 @@ module.exports = {
         value = String(value);
         return value + ' '.repeat(Math.max(length - value.length, 0));
     },
+    toTitleCase: function (str) {
+        return str.replace(
+            /\w\S*/g,
+            function(txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            }
+        );
+    },
     dateStr: function (date) {
         return date.toLocaleString('en-US', { timeZone: 'America/Toronto' });
+    },
+    toPercent: function (num, options) {
+        options = {
+            decimalPlaces: options.decimalPlaces ?? 2,
+            leadingSign: options.leadingSign ?? false
+        }
+        const percentNum = num*100;
+        return `${(percentNum >= 0 && options.leadingSign) ? '+' : ''}${percentNum.toFixed(options.decimalPlaces)}%`;
     }
 };
